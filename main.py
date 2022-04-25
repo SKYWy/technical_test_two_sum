4c7faeff5673f41baa46e4ada5f6097791527969
Katia SHANG <katia.shang@epita.fr>
2022-04-25 15:27:29 +0200

    check_for_sum implemented

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 """
 FUNCTION check_for_sum
-
 Input :
 the_array : python list with integer values
 the_sum_value : integer 
-
 Output : 
 boolean : whether or not there exist two elements in the_array whose sum is exactly the_sum_value.
-
 """
 def check_for_sum(the_array, the_sum_value):
-    
-    ##### ------ YOUR CODE ------ #####
+    the_array.sort()
+
+    for i in range(len(the_array)):
+        for j in range(i + 1, len(the_array)):
+            sum = the_array[i] + the_array[j]
+            
+            if (sum == the_sum_value):
+                return True
+
+            if (sum > the_sum_value):
+                break
+
     return False
 
 
 """
 MAIN
-
 """
 def main():
 
